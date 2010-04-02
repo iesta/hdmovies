b3d2a11303bd8ea2aee7b72ac7d8623300655833
Iesta <sucresemoule@gmail.com>
2010-04-02 14:07:20 +0200

    user model

--- db/migrate/20100402114700_create_users.rb
+++ db/migrate/20100402114700_create_users.rb
@@ -3,8 +3,18 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.string :password
-
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistence_token
+      t.string    :single_access_token, :null => false                
+      t.string    :perishable_token,    :null => false                
+      t.integer   :login_count,         :null => false, :default => 0 
+      t.integer   :failed_login_count,  :null => false, :default => 0 
+      t.datetime  :last_request_at                                    
+      t.datetime  :current_login_at                                   
+      t.datetime  :last_login_at                                      
+      t.string    :current_login_ip                                   
+      t.string    :last_login_ip                                      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100324164534) do
+ActiveRecord::Schema.define(:version => 20100402114700) do
 
   create_table "critics", :force => true do |t|
     t.integer  "user_id"
@@ -43,6 +43,25 @@ ActiveRecord::Schema.define(:version => 20100324164534) do
     t.text     "embed_movie"
     t.string   "apple_url"
     t.string   "amazon_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
+    t.string   "single_access_token",                :null => false
+    t.string   "perishable_token",                   :null => false
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

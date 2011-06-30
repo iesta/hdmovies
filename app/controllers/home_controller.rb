0707c0c2fbb2ccc5e5d7d943c9e5c464ebab2555
Iesta <sucresemoule@gmail.com>
2011-06-30 15:30:54 +0200

    homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @last_movies_new = Movie.order('created_at DESC').limit(15)
-    @last_movies_edit = Movie.order('updated_at DESC').limit(10)
+    @last_movies_new = Movie.order('created_at DESC').limit(14)
+    @last_movies_edit = Movie.order('updated_at DESC').limit(5)
     @last_critics = Critic.order('created_at DESC').limit(15)
     @last_visits = User.order('last_request_at DESC').limit(6)
+    @last_requests = Request.order('created_at DESC').limit(5)
   end
 
   def chat
